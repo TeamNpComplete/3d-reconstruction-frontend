ef05fded036935170da8be646dffcdcb4d17ab70
Imperial-Dragon <adityagunjal1365@gmail.com>
2021-04-23 01:36:52 +0530

    Implemented  onDelete and imahe upload in image preview component

--- src/app/layouts/images-preview/images-preview.component.ts
+++ src/app/layouts/images-preview/images-preview.component.ts
@@ -7,38 +7,36 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class ImagesPreviewComponent implements OnInit {
 
-  @Input('image-url-list') imageUrlList : string[] = [
-    'assets/img/upload/upload_512.png',
-    'assets/img/upload/upload_128.png',
-    'assets/img/upload/upload_512.png',
-    'assets/img/upload/upload_512.png',
-    'assets/img/upload/upload_512.png',
-    'assets/img/upload/upload_512.png',
-    'assets/img/upload/upload_512.png',
-    'assets/img/upload/upload_512.png',
-    'assets/img/upload/upload_512.png',
-    'assets/img/upload/upload_512.png',
-    'assets/img/upload/upload_512.png',
-    'assets/img/upload/upload_512.png',
-    'assets/img/upload/upload_512.png',
-    'assets/img/upload/upload_512.png',
-    'assets/img/upload/upload_512.png',
-    'assets/img/upload/upload_512.png',
-    'assets/img/upload/upload_512.png',
-    'assets/img/upload/upload_512.png',
-    'assets/img/upload/upload_512.png',
-    'assets/img/upload/upload_512.png',
-    'assets/img/upload/upload_512.png',
-    'assets/img/upload/upload_512.png',
-    'assets/img/upload/upload_512.png'
-  ];
+  @Input('image-list') imageList : { name : string, url : string }[] = [];
   constructor() { }
 
   ngOnInit(): void {
   }
 
   onImageDeleteClicked(imageUrl : string) {
-    this.imageUrlList.splice(this.imageUrlList.indexOf(imageUrl), 1);
+    console.log(imageUrl);
+    this.imageList = this.imageList.filter((image : { name : string, url : string }) => image.url !== imageUrl, 1);
   }
 
+  fileBrowseHandler(event : Event) {
+    let files = (event.target as HTMLInputElement).files; 
+
+    if(files) {
+      for(let i = 0; i < files.length; i++) {
+        this.readURL(files[i]);
+      }
+    }
+  }
+
+  readURL(file : File) {
+    let reader = new FileReader();
+    let fileName = file.name;
+
+    reader.readAsDataURL(file);
+
+    reader.onload = (e) => {
+      if(typeof(e.target?.result) === 'string')
+        this.imageList.push({ name : fileName, url : e.target?.result });
+    }
+  }
 }
