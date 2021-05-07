5c1e07b4ce0902f64dc9d09421d3df687c9b549b
Imperial-Dragon <adityagunjal1365@gmail.com>
2021-05-08 00:36:27 +0530

    Improved Image preview

--- src/app/layouts/images-preview/images-preview.component.ts
+++ src/app/layouts/images-preview/images-preview.component.ts
@@ -1,4 +1,8 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { HttpEventType, HttpResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { MeshOptions } from 'angular-stl-model-viewer';
+import { ReconstructionService } from 'src/app/services/reconstruction.service';
+import { Vector3 } from 'three';
 
 @Component({
   selector: 'app-images-preview',
@@ -7,15 +11,28 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class ImagesPreviewComponent implements OnInit {
 
-  @Input('image-list') imageList : { name : string, url : string }[] = [];
-  constructor() { }
+  // Image Preview properties
+  imageList : { name : string, url : string, file : File }[] = [];
+  progress: number = 0;
+  
+  // 3D model properties
+  showModel: boolean = false;
+  modelUrl: string = '';
+  meshOptions: MeshOptions = {
+    scale: new Vector3(0.08, 0.08, 0.08)
+  }
+
+  constructor(private reconstructionService: ReconstructionService) { }
 
   ngOnInit(): void {
   }
 
   onImageDeleteClicked(imageUrl : string) {
-    console.log(imageUrl);
-    this.imageList = this.imageList.filter((image : { name : string, url : string }) => image.url !== imageUrl, 1);
+    this.imageList = this.imageList.filter((image : { name : string, url : string, file : File }) => image.url !== imageUrl, 1);
+  }
+
+  onAddFilesClicked() {
+    document.getElementById('fileDropRef')?.click();
   }
 
   fileBrowseHandler(event : Event) {
@@ -36,7 +53,60 @@ export class ImagesPreviewComponent implements OnInit {
 
     reader.onload = (e) => {
       if(typeof(e.target?.result) === 'string')
-        this.imageList.push({ name : fileName, url : e.target?.result });
+        this.imageList.push({ name : fileName, url : e.target?.result, file : file });
     }
+  }
+
+  onReconstructClicked() {
+    const formData = new FormData();
+    
+    formData.append('userId', '123456');
+    formData.append('modelName', 'Test2');
+
+    for(let i = 0; i < this.imageList.length; i++) {
+      formData.append("files", this.imageList[i].file, this.imageList[i].name);
+    }
+
+    this.reconstructionService.getReconstructedModel(formData).subscribe(
+      (event) => {
+        if (event.type === HttpEventType.UploadProgress) {
+          if(event.total != null)
+            this.progress = Math.round(100 * event.loaded / event.total);
+        } else if (event instanceof HttpResponse) {
+          console.log('Done');
+          let url = window.URL.createObjectURL(event.body);
+          document.getElementById('closeModal')?.click();
+          this.modelUrl = url;
+          this.showModel = true;
+        } else {
+          console.log('Waiting !!!');
+        }
+      },
+
+      (err) => {
+        this.progress = 0;
+        console.log("Failed to upload");
+        console.log(err);
+      }
+    )
+  }
+
+  onDownloadModelClicked() {
+    let popUpBlocked = ('' + window.open).indexOf('[native code]') === -1;
+    var anchor = document.createElement("a");
+    anchor.download = "model.stl";
+    anchor.href = this.modelUrl;
+    anchor.click();
+    if(popUpBlocked) {
+      alert('Allow pop-up to download !');
+    }
+  }
+
+  onSaveModelClicked() {
+
+  }
+
+  intDivideBy3(i: number) {
+    return Math.floor(i / 3);
   }
 }
