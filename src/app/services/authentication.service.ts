1dd8fbe32fc715f545e42a4de108641439d9859b
Imperial-Dragon <adityagunjal1365@gmail.com>
2021-05-08 10:23:42 +0530

    Added authentication  service

---
+++ src/app/services/authentication.service.ts
@@ -0,0 +1,34 @@
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { apiConfiguration } from '../config/api.config';
+
+@Injectable({
+    providedIn : 'root'
+})
+export class AuthenticationService {
+
+    static token : String = "";
+
+    constructor(private http : HttpClient) { }
+
+    login(email : String, password : String) {
+        let requestUrl = apiConfiguration.authenticationHost + apiConfiguration.usersLoginRoute;
+
+        let obj  = {
+            email : email,
+            password : password
+        }
+
+        return this.http.post(requestUrl, obj);
+    }
+
+    register(name : String, email : String, password : String ) {
+        let requestUrl = apiConfiguration.authenticationHost + apiConfiguration.usersRegistrationRoute;
+        
+        return this.http.post(requestUrl, { name : name, email : email, password : password});
+    }
+
+    static isAuthenticated() {
+        return AuthenticationService.token != null && AuthenticationService.token != "";
+    }
+}
