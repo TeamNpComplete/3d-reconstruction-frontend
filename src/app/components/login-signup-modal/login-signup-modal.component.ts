06b095cb848aff30f37c8d3cfbf23dbbc59fd0ba
Imperial-Dragon <adityagunjal1365@gmail.com>
2021-05-10 20:51:08 +0530

    Refactored Login-Signup Modal

--- src/app/components/login-signup-modal/login-signup-modal.component.ts
+++ src/app/components/login-signup-modal/login-signup-modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthenticationService } from 'src/app/services/authentication.service';
@@ -6,13 +6,20 @@ import { AuthenticationService } from 'src/app/services/authentication.service';
 @Component({
   selector: 'app-login-signup-modal',
   templateUrl: './login-signup-modal.component.html',
-  styleUrls: ['./login-signup-modal.component.css']
+  styleUrls: [
+    './login-signup-modal.component.css',
+    '../../../../src/assets/scss/argon.scss'
+  ]
 })
 export class LoginSignupModalComponent implements OnInit {
 
   email!: String;
   password!: String;
   invalidCredentials: Boolean = false;
+  registrationFailed: Boolean = false;
+
+  @Input('tab') tab: string = 'login';
+  @Output('close') close: EventEmitter<void> = new EventEmitter();
 
   constructor(private router: Router, private authenticationService: AuthenticationService) { }
 
@@ -20,25 +27,57 @@ export class LoginSignupModalComponent implements OnInit {
 
   ngOnDestroy() { }
 
-  onSubmit(form: NgForm) {
-    // console.log(form.value);
-    // this.invalidCredentials = false;
-    // let loginSubscription = this.authenticationService.login(form.value.email, form.value.password).subscribe(
-    //   (response) => {
-    //     AuthenticationService.token = response['token'];
-    //     console.log('Authentication Successful !');
-    //     console.log(response);
-    //     loginSubscription.unsubscribe();
-    //     console.log(AuthenticationService.isAuthenticated());
-    //     this.router.navigate(['/dashboard']);
-    //   },
-    //   (error) => {
-    //     this.invalidCredentials = true;
-    //     console.log('Authentication Failed !');
-    //     console.log(error);
-    //     loginSubscription.unsubscribe();
-    //   }
-    // );
+  onLoginSubmit(form: NgForm) {
+    console.log(form.value);
+    this.invalidCredentials = false;
+    let loginSubscription = this.authenticationService.login(form.value.email, form.value.password).subscribe(
+      (response: any) => {
+        AuthenticationService.token = response['token'];
+        console.log('Authentication Successful !');
+        console.log(response);
+        loginSubscription.unsubscribe();
+        console.log(AuthenticationService.isAuthenticated());
+        this.router.navigate(['/dashboard']);
+      },
+      (error) => {
+        this.invalidCredentials = true;
+        console.log('Authentication Failed !');
+        console.log(error);
+        loginSubscription.unsubscribe();
+      }
+    );
+  }
+
+  onSignupSubmit(form: NgForm) {
+    console.log(form.value);
+    this.registrationFailed = false;
+    let registrationSubscription = this.authenticationService.register(form.value.name, form.value.email, form.value.password).subscribe(
+      (response: any) => {
+        AuthenticationService.token = response['token'];
+        console.log('Registration Successful !');
+        console.log(response);
+        registrationSubscription.unsubscribe();
+        this.router.navigate(['/dashboard']);
+      },
+      (error) => {
+        this.registrationFailed = true;
+        console.log('Registration Failed !');
+        registrationSubscription.unsubscribe();
+        console.log(error);
+      }
+    )
+  }
+
+  changeTab() {
+    if(this.tab === 'login') {
+      this.tab = 'signup';
+    } else {
+      this.tab = 'login';
+    }
+  }
+
+  onBackdropClicked() {
+    this.close.emit();
   }
 
 }
